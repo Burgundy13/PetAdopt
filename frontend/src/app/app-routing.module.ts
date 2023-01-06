1f66b0e97e8e20524326e59ae934be22b2131ec8
Vlado Ivkovic <ivkovicv90@gmail.com>
2023-01-06 22:55:38 +0100

    service add and routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdoptionsComponent } from './adoptions/adoptions.component';
+import { HomeComponent } from './core/home/home.component';
+import { PetsComponent } from './pets/pets.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'pets', component: PetsComponent },
+  { path: 'adoptions', component: AdoptionsComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
