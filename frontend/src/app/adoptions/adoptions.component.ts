b638e39453903d7b91e5ede93d0a4fdc8bd19616
Vlado Ivkovic <ivkovicv90@gmail.com>
2023-01-10 19:22:05 +0100

    adoption componenet

--- frontend/src/app/adoptions/adoptions.component.ts
+++ frontend/src/app/adoptions/adoptions.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { AdoptionList } from '../model/adoption';
+import { PetService } from '../service/pet.service';
 
 @Component({
   selector: 'app-adoptions',
   templateUrl: './adoptions.component.html',
-  styleUrls: ['./adoptions.component.css']
+  styleUrls: ['./adoptions.component.css'],
 })
 export class AdoptionsComponent implements OnInit {
+  adoptionList: AdoptionList = new AdoptionList();
 
-  constructor() { }
+  constructor(private service: PetService) {}
 
   ngOnInit(): void {
+    this.getAll();
   }
 
+  getAll(): void {
+    this.service.getAllAdoptions().subscribe({
+      next: (response: AdoptionList) => {
+        this.adoptionList = response;
+      },
+      error: (response: any) => {
+        console.log('Error', response.statusText);
+      },
+    });
+  }
 }
